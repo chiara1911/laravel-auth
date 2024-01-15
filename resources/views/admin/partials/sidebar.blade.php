bb0be1eaf5d86c457fd0ac38cb7509b1fb70ac97
chiara1911 <chiaracesari89@gmail.com>
2024-01-15 09:32:10 +0100

    fix page home.blade

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,9 +2,9 @@
     <nav class="d-flex flex-column m-3 ">
         <!-- first-top-bar -->
 
-                <h2 class="text-light mb-5">Pagine</h2>
-                 <p><a href="{{ route('admin.projects.create') }}" class="text-light text-decoration-none my-3">Crea Nuovo Progetto</a></p>
-                 <p><a href="{{route('admin.projects.index')}}" class=" my-3 text-light">  Pagina principale</a></p>
+                <h2 class="text-light m-3">Pagine</h2>
+                 <p><a href="{{ route('admin.projects.create') }}" class="text-light text-decoration-none m-3">Crea Nuovo Progetto<i class="fa-solid fa-circle-plus m-3"></i></a></p>
+                 <p><a href="{{route('admin.projects.index')}}" class=" m-3 text-light">  Pagina principale</a></p>
 
     </nav>
 </div>
